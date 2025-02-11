48533579b956f7f470ac7c2aa2586f0a70bf6556
sagariyavaishnavi <sagariyavaishnavi@gmail.com>
2025-02-11 20:02:21 +0530

    Task2 added

---
+++ Task2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Greet from './components/Greet'
+const App = () => {
+  return (
+    <>
+      <Greet/>
+    </>
+  )
+}
+
+export default App;
---
+++ Task2/src/components/Greet.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greet() {
+  return <h1>Hello, Vaishnavi !</h1>;
+}
+
+export default Greet;
