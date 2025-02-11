1fc0d0d2e262fb21d7974b788696db00747d5846
sagariyavaishnavi <sagariyavaishnavi@gmail.com>
2025-02-11 21:29:01 +0530

    Task5 added

---
+++ Task5/src/components/JSXRules.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function JSXRules() {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p>Here are some important JSX rules:</p>
+      <ol>
+        <li>JSX must return a single parent element.</li>
+        <li>JSX elements must be properly closed.</li>
+        <li>JSX attributes use camelCase .</li>
+      </ol>
+    </div>
+  );
+}
+
+export default JSXRules;
